b23bbc4324407f5484d82321dacb5c0e62314fc9
naburov <burov.nikita42@gmail.com>
2018-11-22 21:31:05 +0500

    фывапролдж

--- Тесты/UnitTest1.cs
+++ Тесты/UnitTest1.cs
@@ -11,7 +11,6 @@ namespace Тесты
     {
         
         string place = "D:\\Учёба\\2 курс\\Алгоритмы и структуры данных\\Максимальный поток\\Создание тестовых файлов\\bin\\Debug";
-        static int count = 2;
 
         private static int Test(string filename)
         {
@@ -26,10 +25,111 @@ namespace Тесты
             sr.ReadLine();
             return int.Parse(sr.ReadLine());
         }
+
         [TestMethod]
         public void TestMethod1()
         {
+            int count = 2;
             string filename = place+"\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int count = 3;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int count = 4;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int count = 5;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int count = 6;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int count = 7;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int count = 8;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int count = 9;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int count = 10;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
+            string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
+            int l = Test(filename);
+            int a = GetAnswer(filenameAns);
+            Assert.AreEqual(l, a);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int count = 11;
+            string filename = place + "\\Mixed Matrix" + String.Format(" {0}.txt", count);
             string filenameAns = place + "\\Properties" + String.Format(" {0}.txt", count++);
             int l = Test(filename);
             int a = GetAnswer(filenameAns);
